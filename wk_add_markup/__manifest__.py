6cf713c0b56382543745e17329524ffd6845987b
Neha Gupta <neha.gupta453@webkul.com>
2018-04-30 12:34:37 +0530

    first commit

---
+++ wk_add_markup/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#################################################################################
+{
+    "name": "Order Line Markup Field",
+    "category": 'Marketing' ,
+    "summary": """""",
+    "sequence": 10,
+    "author": "Webkul Software Pvt. Ltd.",
+    "license" :  "Other proprietary",
+    "website": "http://www.webkul.com",
+    "depends": ['sale_management'],
+    "description" :  """""",
+    "data": [
+        'view/markup_view.xml',
+
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+
+}
---
+++ wk_add_markup/models/markup_field.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+#
+#    Copyright (c) 2017-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+#
+##########################################################################
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+class SaleOrderLine(models.Model):
+
+    _inherit = "sale.order.line"
+
+    markup = fields.Float(string="Mark up (In percetage)")
+
+    @api.multi
+    @api.onchange('product_id','markup')
+    def change_product_markup(self):
+        if self.product_id and self.markup:
+            raise UserError(self.product_id.standard_price)
+            self.price_unit = self.product_id.standard_price*(1+self.markup)
